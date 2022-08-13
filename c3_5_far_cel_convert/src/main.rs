5e4529cdf1185768c860ada8e21a74a100d21d21
PerryCameron <pcameron28@yahoo.com>
2022-08-12 21:54:23 -0400

    finished first exercise

--- c3_5_far_cel_convert/src/main.rs
+++ c3_5_far_cel_convert/src/main.rs
@@ -1,3 +1,13 @@
+
+// https://doc.rust-lang.org/book/ch03-05-control-flow.html
+// Convert temperatures between Fahrenheit and Celsius.
+
+// Celsius to Fahrenheit Formula: (°C * 1.8) + 32 = °F
+//
+// Fahrenheit to Celsius Formula: (°F - 32) / 1.8 = °C
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("87.6 degrees fahrenheit is {:.1} Celsius", (87.6 - 32.0) / 1.8);
+    println!("28.2 degrees celsius is {:.1} fahrenheit", (28.2 * 1.8) + 32.0);
 }
