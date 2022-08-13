ce6ef4f6e7295216129e530c015a34d0f2ba704b
PerryCameron <pcameron28@yahoo.com>
2022-08-12 22:16:19 -0400

    made slight improvment so you can see fibonacci

--- c3_5_2nth_fibonacci/src/main.rs
+++ c3_5_2nth_fibonacci/src/main.rs
@@ -12,5 +12,7 @@ fn fib(n: i64) -> i64 {
     if n <= 1 {
         return n;
     }
-    return fib(n -1) + fib(n -2)
+    let answer = fib(n -1) + fib(n -2);
+    println!("fib({} - 1) + fib({} - 2) = {}", n, n, answer);
+    answer
 }
