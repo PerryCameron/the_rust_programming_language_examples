ecf2ecdf46be9ef5a40346e98f4c36cca82ef524
PerryCameron <pcameron28@yahoo.com>
2022-08-13 14:35:13 -0400

    finished 1.2-1

--- rbe_2_2-1_display_matrix/src/main.rs
+++ rbe_2_2-1_display_matrix/src/main.rs
@@ -1,3 +1,27 @@
+
+// Recap: Add the fmt::Display trait to the Matrix struct in the above example, so that if
+// you switch from printing the debug format {:?} to the display format {},
+// you see the following output:
+//
+// ( 1.1 1.2 )
+// ( 2.1 2.2 )
+
+// Add a transpose function using the reverse function as a template, which accepts a matrix as
+// an argument, and returns a matrix in which two elements have been swapped. For example:
+//
+//
+// println!("Matrix:\n{}", matrix);
+// println!("Transpose:\n{}", transpose(matrix));
+// results in the output:
+//
+//
+// Matrix:
+// ( 1.1 1.2 )
+// ( 2.1 2.2 )
+// Transpose:
+// ( 1.1 2.1 )
+// ( 1.2 2.2 )
+
 // Import (via `use`) the `fmt` module to make it available.
 use std::fmt;
 
